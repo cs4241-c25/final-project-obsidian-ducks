719bd6930d0dde63209222e4c5d2a358a968d1f1
tacheampong <th.acheampong@gmail.com>
2025-02-19 09:35:52 -0500

    get items from database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,52 @@
+'use client'
 import Link from "next/link";
 
 import Button from "@/components/Button";
 import { FileUploadTest } from "@/components/FileUploadTest";
+import {useEffect, useState} from "react";
+import item from "../../models/Item";
 
 export default function Home() {
+    const [items, setItems] = useState([])
+    useEffect(() => {
+        const fetchItems = async () => {
+            const res = await fetch('/api/items')
+            const items = await res.json()
+
+            if(items.status == 200){
+                setItems(items.data)
+            }
+            else {
+                console.log(items.error)
+            }
+        }
+        fetchItems()
+    }, []);
     return (
         <main>
             <div>Wpi buys</div>
+{/*
             <FileUploadTest/>
+*/}
             <Link href="/sell">
                 <Button>Sell an Item</Button>
             </Link>
             <Link href="/profile">
                 <Button>Profile</Button>
             </Link>
+            <div>
+
+                {items.map((item, index) => (
+                    <div key={index}>
+                        <h2>{item.title}</h2>
+                        <h2>{item.description}</h2>
+                        <h2>{item.category}</h2>
+                        <h2>{item.price}</h2>
+
+                    </div>
+
+                ))}
+            </div>
         </main>
     );
 }
