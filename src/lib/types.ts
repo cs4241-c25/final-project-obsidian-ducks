4621540cf382b3cc90f08624017b1794428dca6e
nick-leslie <nick.leslie303@gmail.com>
2025-02-19 12:40:49 -0500

    moved types to be in sync

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -16,3 +16,13 @@ export const ITEM_CATEGORIES = [
     ItemCategory.HandMade
 ];
 
+
+export type Event = "REGISTER" | "MESSAGE"
+
+export type Message = {
+  event:Event
+  message:string,
+  sender:string,//todo change this to an id?
+  recver:string,//todo change this to a user id or chat id?
+  read:boolean
+}
--- src/types.d.ts
+++
@@ -1,9 +0,0 @@
-export type Event = "REGISTER" | "MESSAGE"
-
-export type Message = {
-  event:Event
-  message:string,
-  sender:string,//todo change this to an id?
-  recver:string,//todo change this to a user id or chat id?
-  read:boolean
-}
