cffebd23a85d7b9c1ada881aa6fa38ed710ca13e
tacheampong <th.acheampong@gmail.com>
2025-02-19 09:39:26 -0500

    added onChange for selectInput

--- src/components/SelectInput.tsx
+++ src/components/SelectInput.tsx
@@ -1,16 +1,29 @@
-import { ReactNode } from "react";
+'use client'
+import {ChangeEventHandler, ReactNode, useState} from "react";
 
 interface SelectInput {
     children: ReactNode;
+    onChange?: ChangeEventHandler<HTMLSelectElement>;
     name: string;
     options: string[];
 }
 
 export default function SelectInput(props: SelectInput) {
+    const [selectedOption, setSelectedOption] = useState<string>(props.options[0] || ''); // Default to the first option if available
+
+    const handleSelectChange: ChangeEventHandler<HTMLSelectElement> = (e) => {
+        setSelectedOption(e.target.value);  // Update the local state
+        if (props.onChange) {
+            props.onChange(e);
+        }
+    };
+
     return (
-        <label className={"flex flex-col border"}>
+
+        <label  className={"flex flex-col border"}>
             {props.children}
-            <select name={props.name}>
+            <select defaultValue="" onChange={handleSelectChange} name={props.name}>
+                <option value=""  disabled={true}>Select an Option</option>
                 {props.options.map(option =>
                     <option key={option} value={option}>{option}</option>
                 )}
