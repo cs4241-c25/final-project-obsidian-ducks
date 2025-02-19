7c12a87941ba2a1d7deb11d7373929cf0d534ce0
tacheampong <th.acheampong@gmail.com>
2025-02-19 09:38:28 -0500

    added onChange for TextInput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,17 +1,20 @@
-import { ReactNode } from "react";
+import React, {ChangeEventHandler, ReactNode} from "react";
 
 interface TextInput {
     children: ReactNode;
+    onChange?: ChangeEventHandler<HTMLInputElement>
     type: string;
     name: string;
     placeholder: string;
 }
 
 export default function TextInput(props: TextInput) {
+
+
     return (
         <label className={"flex flex-col border"}>
             {props.children}
-            <input type={props.type} name={"props.name"} placeholder={props.placeholder}/>
+            <input onChange={props.onChange} type={props.type} name={"props.name"} placeholder={props.placeholder}/>
         </label>
     );
 }
