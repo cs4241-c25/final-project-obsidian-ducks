0b1d0635577003f7308210e3f654fc695a96ace3
tacheampong <th.acheampong@gmail.com>
2025-02-19 09:34:44 -0500

    added and onclick event handler to button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,13 @@
-import { ReactNode } from "react";
+import React, { ReactNode } from "react";
 
 interface Button {
-  children: ReactNode
+  children: ReactNode;
+  onClick?: React.MouseEventHandler<HTMLButtonElement>
 }
 
 export default function Button(props: Button) {
   return (
-      <button className={"bg-auburn-500 rounded-lg w-fit py-2 px-4 text-center text-white hover:bg-auburn-600"}>
+      <button onClick={props.onClick} className={"bg-auburn-500 rounded-lg w-fit py-2 px-4 text-center text-white hover:bg-auburn-600"}>
         {props.children}
       </button>
   )
