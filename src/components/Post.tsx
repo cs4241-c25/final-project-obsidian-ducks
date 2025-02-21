dd92eb955825fea6cf25c1896939d91586c6ff08
cnguyen111 <cnguyen1@wpi.edu>
2025-02-21 18:13:38 -0500

    Added Post Component with type label

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -18,13 +18,13 @@ export default function Post(props: PostInput) {
             <div className="w-[300px] h-[340px] shadow-xl">
                 <div className="relative h-[250px]">
                     <Link href={`/post/${props._id}`}>
-                        <Button>
+                        <Button type="button">
                     <Image priority={false}
                            src={props.image}
                            fill alt="Test" style={{objectFit: "cover"}}/>
                         </Button>
                     </Link>
-                    <Button className="absolute top-2 right-2 z-10 p-2 rounded-full">
+                    <Button type="button" className="absolute top-2 right-2 z-10 p-2 rounded-full">
                         <Image src="/heart.svg" alt="Heart Image" width={15} height={15}/>
                     </Button>
                 </div>
