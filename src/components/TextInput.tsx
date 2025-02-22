a0f2d47bfef955399d0212668d53a217d55df04f
Andy Truong <andyhtruong01@gmail.com>
2025-02-22 11:52:55 -0500

    tw-merge to textinput

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -1,18 +1,19 @@
 import { ReactNode } from "react";
+import { twMerge } from "tailwind-merge";
 
 interface TextInput {
+    className: string;
     children: ReactNode;
     type: string;
     name: string;
     placeholder: string;
-    width?: string;
 }
 
 export default function TextInput(props: TextInput) {
     return (
         <label className="flex flex-col">
             {props.children}
-            <input className={ (props.width ? props.width : "w-fit") + " h-10 border rounded-sm px-2 py-1" }
+            <input className={twMerge("w-fit h-10 border rounded-sm px-2 py-1", props.className)}
                    type={props.type}
                    name={props.name}
                    placeholder={props.placeholder}/>
