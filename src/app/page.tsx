ff6035e9b53df46f9ce2182af9b682987ab3f3fd
Andy Truong <andyhtruong01@gmail.com>
2025-02-24 15:53:05 -0500

    homepage is responsive to all devices

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,11 +10,18 @@ export default function Home() {
             <div className="w-screen relative">
                 <div className="absolute w-full h-full bg-linear-to-r from-crimson-500 to-crimson-500 opacity-50"></div>
                 <img className="w-full" src="/WPI.png" alt="WPI campus"/>
-                <p className={`absolute top-[30%] left-[3%] ${oswald.className} text-6xl tracking-widest text-center leading-[65px] font-semibold text-white`}>
-                    Sell, Buy, and Swap<br/>
-                    Secondhand Items with<br/>
-                    Fellow WPI Students
-                </p>
+                <div className="absolute top-0 left-0 w-45/100 h-full flex justify-center items-center">
+                    <p className={`${oswald.className} inline text-center text-white opacity-95 
+                    md:font-medium lg:font-semibold 
+                    text-lg sm:text-xl md:text-2xl lg:text-3xl xl:text-4xl 2xl:text-5xl 
+                    tracking-wider lg:tracking-widest xl:tracking-[0.15em] 2xl:tracking-[0.2em]
+                    leading-[30px] md:leading-[45px] xl:leading-[55px] 2xl:leading-[75px] 
+                    `}>
+                        Sell, Buy, and Swap<br/>
+                        Secondhand Items with<br/>
+                        Fellow WPI Students
+                    </p>
+                </div>
             </div>
         </main>
     );
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,7 +5,7 @@ import Button from "@/components/Button";
 export default function NavBar(){
     return (
         <nav className="w-screen h-[10vh] flex justify-between items-center border">
-            <div className="flex h-full justify-between items-center gap-8 text-lg font-semibold ml-10">
+            <div className="flex h-full justify-between items-center gap-8 md:text-md lg:text-lg font-semibold ml-10">
                 <Link href="/profile">
                     <p>Login</p>
                 </Link>
@@ -14,12 +14,12 @@ export default function NavBar(){
                 </Link>
             </div>
             <Link href="/">
-                <svg width={85} height="100%" viewBox="0 0 100 100">
+                <svg className="xl:w-[75px] 2xl:w-[85px]" width={70} height="100%" viewBox="0 0 100 100">
                     <image href="/WPIBuys.png" />
                 </svg>
             </Link>
             <Link href="/sell">
-                <Button className="mr-10" type="button">Sell an Item</Button>
+                <Button className="mr-10 md:text-md lg:text-lg" type="button">Sell an Item</Button>
             </Link>
         </nav>
     );
