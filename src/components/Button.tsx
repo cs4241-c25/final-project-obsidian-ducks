65914505ac053a06a6337587db03ab2864b0abb7
Andy Truong <andyhtruong01@gmail.com>
2025-02-24 14:58:40 -0500

    Adjusted crimson palette to add clarity to components

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -12,7 +12,7 @@ export default function Button(props: Button) {
   return (
       <button type={props.type}
               className={twMerge(
-                  "bg-crimson-500 hover:bg-crimson-600 rounded-lg w-fit py-2 px-4 text-center text-white cursor-pointer",
+                  "bg-crimson-500 hover:bg-crimson-700 rounded-lg w-fit py-2 px-4 text-center text-white cursor-pointer",
                   props.className
               )}
               onClick={props.onClick}
