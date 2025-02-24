177c00948116b623f4383ee2dde47864a930b7f8
tacheampong <th.acheampong@gmail.com>
2025-02-24 16:19:09 -0500

    changed userID type to string added default for bool

--- src/models/Like.ts
+++ src/models/Like.ts
@@ -1,9 +1,9 @@
 import mongoose, {mongo, Schema} from 'mongoose'
 
 const LikeSchema = new mongoose.Schema({
-    itemID: {type: mongoose.Schema.Types.ObjectId, ref: 'Item'},
-    userId: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
-    isLiked: {type: Boolean}
+    itemID: {type: mongoose.Schema.Types.ObjectId, ref: 'Item', required: true},
+    username: {type: String, ref: 'User'},
+    isLiked: {type: Boolean, default: false}
 
 })
 const Like = mongoose.models.Like || mongoose.model('Like', LikeSchema)
