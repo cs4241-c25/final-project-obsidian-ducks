f90051a3afcdba47ee0a49f322566a79a2ea55ce
Andy Truong <andyhtruong01@gmail.com>
2025-02-24 17:46:44 -0500

    sell page now responsive to all devices

--- src/app/sell/ListingForm.tsx
+++ src/app/sell/ListingForm.tsx
@@ -22,14 +22,14 @@ async function postItem(formData: FormData) {
 export default function ListingForm() {
     return (
         <form className="w-3/5" action={postItem}>
-            <h1 className="font-bold text-2xl text-center mb-14">Sell an Item</h1>
-            <div className="flex justify-center gap-6">
-                <FileDropzone className="w-1/2"/>
+            <h1 className="font-bold text-2xl text-center mb-8 2xl:mb-14">Sell an Item</h1>
+            <div className="flex flex-col 2xl:flex-row justify-center gap-6">
+                <FileDropzone className="w-full h-1/4 2xl:w-1/2 2xl:h-140"/>
                 <div className="flex flex-col gap-3">
-                    <TextInput className="w-100" type="text" name="title" placeholder="What are you selling?">Title</TextInput>
-                    <TextInput className="w-25" type="number" name="price" placeholder="5.99">Price</TextInput>
+                    <TextInput className="2xl:w-100" type="text" name="title" placeholder="What are you selling?">Title</TextInput>
+                    <TextInput className="2xl:w-25" type="number" name="price" placeholder="5.99">Price</TextInput>
                     <SelectInput name="category" options={ITEM_CATEGORIES}>Category</SelectInput>
-                    <TextArea className="w-100 h-60" name="description" placeholder="E.g., Recently bought new. ">Description</TextArea>
+                    <TextArea className="w-full h-25 2xl:w-100 2xl:h-60" name="description" placeholder="E.g., Recently bought new. ">Description</TextArea>
                     <div className="flex gap-2">
                         <Button className="w-25 grow bg-gray-600 hover:bg-gray-800" type="reset">Reset</Button>
                         <Button className="w-25 grow" type="submit">Submit</Button>
