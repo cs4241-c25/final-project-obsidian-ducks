23300414284f8ff3f4d3d5a509e66f5bb77335d3
Andy Truong <andyhtruong01@gmail.com>
2025-02-24 02:22:42 -0500

    added an edited version of hero image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
         <main>
             <div className="w-screen relative">
                 <div className="absolute w-full h-full bg-linear-to-r from-crimson-500 to-crimson-500 opacity-50"></div>
-                <img className="w-full" src="/WPI.jpg" alt="WPI campus"/>
+                <img className="w-full" src="/WPI.png" alt="WPI campus"/>
                 <p className={`absolute top-[30%] left-[3%] ${oswald.className} text-6xl tracking-widest text-center leading-[65px] font-semibold text-white`}>
                     Sell, Buy, and Swap<br/>
                     Secondhand Items with<br/>
