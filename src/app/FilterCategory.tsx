8cf74e78c2497cacdeb18dedd06093cf4edb0660
cnguyen111 <cnguyen1@wpi.edu>
2025-02-25 10:46:02 -0500

    fixed search and category filtering bugs

--- src/app/FilterCategory.tsx
+++ src/app/FilterCategory.tsx
@@ -1,18 +1,16 @@
-import { ChangeEventHandler } from "react";
+import React from "react";
 
 interface FilterCategory {
     type: string;
     name: string;
-    onChange: ChangeEventHandler<HTMLInputElement>;
+    onChange: React.ChangeEventHandler<HTMLInputElement>;
 }
 
 export default function FilterCategory(props: FilterCategory){
     return (
-        <label className={`px-4 py-2 w-fit rounded-lg shadow-md hover:bg-auburn-100 hover:scale-110 duration-125 ease-in-out}`}>
-            <input type={props.type} name={props.name} value={props.name} onChange={props.onChange}/>
-            <span className="pl-2">
-                {props.name}
-            </span>
+        <label className={`px-4 py-2 w-fit rounded-lg shadow-md hover:bg-auburn-100 hover:scale-110 duration-125 ease-in-out has-[:checked]:scale-110}`}>
+            <input type={props.type} name={props.name} value={props.name} onChange={props.onChange} className=""/>
+            {props.name}
         </label>
     );
 }
--- src/app/ItemsSection.tsx
+++ src/app/ItemsSection.tsx
@@ -4,6 +4,8 @@ import { ChangeEvent, useRef, useState } from "react";
 import FilterCategory from "./FilterCategory";
 import ItemPost from "./ItemPost";
 import SearchBar from "./SearchBar";
+import PriceInput from "@/components/PriceInput"
+
 
 import { Item, ITEM_CATEGORIES } from "@/lib/types";
 
@@ -15,47 +17,94 @@ export default function ItemsSection(props: ItemPosts) {
     const [items, setItems] = useState(props.items);
     const filters = useRef<string[]>([]);
     const query = useRef<string>("");
+    const [search, setSearch] = useState<string>("");
+    const [filteredPrice, setFilteredPrice] = useState({
+        min: 0,
+        max: 500,
+    });
 
     function filterBySearch(e: ChangeEvent<HTMLInputElement>) {
-        query.current = e.target.value;
-        const searchedItems = props.items.filter(item => {
-            if (filters.current.length != 0)
-                return item.title.includes(e.target.value) && filters.current.includes(item.category);
-            else
-                return item.title.includes(e.target.value);
-        });
-        setItems(searchedItems);
+        setSearch(e.target.value);
+        if(e.target.value === "" && filters.current.length === 0) {
+            setItems(props.items);
+        } else if (filters.current.length !== 0) {
+            if(e.target.value === "") {
+                const searchedItems = props.items.filter(item => {
+                    return filters.current.includes(item.category);
+                });
+                setItems(searchedItems);
+            } else {
+                const searchedItems = props.items.filter(item => {
+                    return item.title.includes(e.target.value) && filters.current.includes(item.category);
+                });
+                setItems(searchedItems);
+            }
+        }
     }
 
     function filterByCategory(e: ChangeEvent<HTMLInputElement>) {
+        setSearch("");
         if (e.target.checked) {
-            if (!filters.current.includes(e.target.value)) {
-                filters.current.push(e.target.value); // Update filters list
-            }
-            const filteredItems = props.items.filter(item => filters.current.includes(item.category) && item.title.includes(query.current)); // Apply filter categories
+            filters.current.push(e.target.value); // Update filters list
+            const filteredItems = props.items.filter(item => filters.current.includes(item.category)); // Apply filter categories
             setItems(filteredItems);
-        } else {
-            filters.current = filters.current.filter(filter => filter === e.target.value);
-            setItems(props.items);
+        } else if (!e.target.checked){
+                filters.current = filters.current.filter(filter => filter !== e.target.value);
+                if(filters.current.length === 0) {
+                    setItems(props.items);
+                } else {
+                const filteredItems = props.items.filter(item => filters.current.includes(item.category)); // Apply filter categories
+                setItems(filteredItems);
+            }
         }
     }
+
+    function handlePrice(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch("");
+        if (e.target.name === "min") {
+            if(e.target.value === ""){
+                setFilteredPrice(prevVal => ({...prevVal, min: 0}));
+            } else {
+                setFilteredPrice(prevVal => ({...prevVal, min: Number(e.target.value)}));
+            }
+        } else if (e.target.name === "max"){
+            if(e.target.value === ""){
+                setFilteredPrice(prevVal => ({...prevVal, max: 500}));
+            } else {
+                setFilteredPrice(prevVal => ({...prevVal, max: Number(e.target.value)}));
+            }
+        }
+    }
+
 
     return (
         <section>
             <div className="flex justify-center">
-                <SearchBar onChange={filterBySearch} />
+                <SearchBar value={search} onChange={filterBySearch}/>
             </div>
-            <div className="flex justify-center">
-                <div className="flex mr-8 gap-x-2">
+            <div className="flex justify-center gap-x-2 py-4">
                     {ITEM_CATEGORIES.map((category) => (
                         <FilterCategory key={category} type={"checkbox"} name={category} onChange={filterByCategory}/>
                     ))}
-                </div>
+                    <div className="flex gap-x-1">
+                        <h1 className="place-self-center">$</h1>
+                        <span>
+                        <PriceInput className="w-17" type={"number"} name={"min"} placeholder={"Min"}
+                                    onChange={handlePrice}></PriceInput>
+                    </span>
+                        <h1 className="content-center px-2">-</h1>
+                        <h1 className="place-self-center">$</h1>
+                        <span>
+                        <PriceInput className="w-17" type={"number"} name={"max"} placeholder={"Max"}
+                                    onChange={handlePrice}></PriceInput>
+                    </span>
+                    </div>
             </div>
             <div className="flex justify-center">
                 <div className="w-3/4 flex flex-wrap gap-9.5">
                     {items.map((item) =>
-                        <ItemPost key={item.id} id={item.id} title={item.title} category={item.category} price={item.price} image={item.image}/>)}
+                        <ItemPost key={item.id} id={item.id} title={item.title} category={item.category}
+                                  price={item.price} image={item.image}/>)}
                 </div>
             </div>
         </section>
