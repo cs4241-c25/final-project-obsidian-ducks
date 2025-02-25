bf4a234212bc17253f788e53ef0a96a0874e3c95
cnguyen111 <cnguyen1@wpi.edu>
2025-02-24 22:31:28 -0500

    MULTIPARAMETER FILTERING!!!!!!

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,13 +20,11 @@ export default function Home() {
     const [posts, setPosts] = useState<Post[]>([]);
     const [originalPosts, setOriginalPosts] = useState<Post[]>([]);
     const [filters, setFilters] = useState<string[]>([]);
-    // const [prevState, setPrevState] = useState([]);
-    // const [filteredList, setFilteredList] = useState([]);
-    // const [filteredCategory, setFilteredCategory] = useState([]);
-    // const [filteredPrice, setFilteredPrice] = useState({
-    //     min: 0,
-    //     max: 500,
-    // });
+    const [search, setSearch] = useState<string>("");
+    const [filteredPrice, setFilteredPrice] = useState({
+        min: 0,
+        max: 500,
+    });
 
     async function getPosts() {
         try {
@@ -44,43 +42,65 @@ export default function Home() {
     }
 
     async function handleFiltered(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch("");
         if (e.target.checked) {
             const newFilters = [e.target.value, ...filters];
             setFilters(newFilters);
-            let filtered = originalPosts.filter((originalPost) => {
-                return newFilters.includes(originalPost.category);
-            });
-            console.log(filtered);
-            // filtered = posts.filter(originalPost =>
-            //     filtered.some(filteredPost => originalPost._id == filteredPost._id)
-            // );
-            setPosts(filtered);
+        } else if (!e.target.checked){
+            let filteredCategories = filters.filter((category) => category !== e.target.name);
+            setFilters(filteredCategories);
         }
+    }
 
-        if (!e.target.checked) await getPosts();
+    async function handlePrice(e: React.ChangeEvent<HTMLInputElement>) {
+        setSearch("");
+        if (e.target.name === "min") {
+            if(e.target.value === ""){
+                setFilteredPrice(prevVal => ({...prevVal, min: 0}));
+            } else {
+                setFilteredPrice(prevVal => ({...prevVal, min: Number(e.target.value)}));
+            }
+        } else if (e.target.name === "max"){
+            if(e.target.value === ""){
+                setFilteredPrice(prevVal => ({...prevVal, max: 500}));
+            } else {
+                setFilteredPrice(prevVal => ({...prevVal, max: Number(e.target.value)}));
+            }
+        }
     }
 
     async function handleSearch(e: React.ChangeEvent<HTMLInputElement>){
-        const filtered = posts.filter((post) => post.title.toLowerCase().includes(e.target.value.toLowerCase()));
+        const searchWord = e.target.value.toLowerCase();
+        setSearch(searchWord);
+        const filtered = posts.filter((post) => post.title.toLowerCase().includes(searchWord));
         setPosts(filtered);
-
         if (e.target.value === "") await getPosts();
     }
 
     useEffect(() => {
-        getPosts().then();
+        getPosts().then(() =>{
+        });
     }, []);
 
+    useEffect(() => {
+        if (filters.length > 0) {
+            let newData = originalPosts.filter(post => filters.includes(post.category));
+            setPosts(newData.filter((item) => {return item.price >= filteredPrice.min && item.price <= filteredPrice.max}));
+        } else {
+            let newData = originalPosts.filter((item) => {return item.price >= filteredPrice.min && item.price <= filteredPrice.max})
+            setPosts(newData);
+        }
+    }, [filters, filteredPrice, originalPosts]);
 
     return (
         <main>
             <div className="grid grid-cols-1 md:grid-cols-4 w-full">
                 <div className="w-full md:w-70 h-auto pl-4 pt-4 border-r-2">
-                    <SearchBar onChange={handleSearch}/>
+                    <SearchBar onChange={handleSearch} value={search}/>
                     <div className="flex flex-col mr-8 gap-y-2">
                         <label className="font-bold text-xl pt-4">Categories</label>
                         {ITEM_CATEGORIES.map((category) => (
-                            <FilterCategory key={category} type={"checkbox"} name={category} onChange={handleFiltered}/>
+                            <FilterCategory key={category} type={"checkbox"} name={category} value={search} onChange={handleFiltered}/>
                         ))}
                     </div>
                     <div className="flex flex-col mr-8">
@@ -88,12 +108,12 @@ export default function Home() {
                         <div className="flex pt-2 gap-x-1">
                             <h1 className="place-self-center">$</h1>
                             <span>
-                            <TextInput className="w-17" type={"number"} name={"min"} placeholder={"Min"} onChange={handleFiltered}></TextInput>
+                            <TextInput className="w-17" type={"number"} name={"min"} placeholder={"Min"} onChange={handlePrice}></TextInput>
                             </span>
                             <h1 className="content-center px-2">-</h1>
                             <h1 className="place-self-center">$</h1>
                             <span>
-                            <TextInput className="w-17" type={"number"} name={"max"} placeholder={"Max"} onChange={handleFiltered}></TextInput>
+                            <TextInput className="w-17" type={"number"} name={"max"} placeholder={"Max"} onChange={handlePrice}></TextInput>
                                 </span>
                         </div>
                     </div>
