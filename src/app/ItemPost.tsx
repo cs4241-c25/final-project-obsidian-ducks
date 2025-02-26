18ecaa795fe1b34e678807f43f5e87041be77958
cnguyen111 <cnguyen1@wpi.edu>
2025-02-26 09:34:56 -0500

    Made home page responsive and finished styling

--- src/app/ItemPost.tsx
+++ src/app/ItemPost.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Button from "@/components/Button";
+
 interface ItemPost {
     id: string;
     title: string;
@@ -9,9 +12,17 @@ interface ItemPost {
 export default function ItemPost(props: ItemPost) {
     return (
         <figure className="">
-            <img className="w-[250px] h-[250px] rounded-sm object-cover" src={props.image} alt={props.title} />
+            <div className="relative">
+                <Link href={`/listing/${props.id}`}>
+                <img className="w-[250px] h-[250px] rounded-sm object-cover hover:scale-105 duration-150 ease-in-out" src={props.image} alt={props.title} />
+                </Link>
+                <Button type="button" className="absolute top-2 right-2 z-10 p-2 rounded-full shadow-md bg-white hover:bg-auburn-300">
+                    <img src="/like.svg" alt="Heart Image" width={15} height={15}/>
+                </Button>
+            </div>
             <figcaption>
                 <p className="font-semibold tracking-wider">${props.price}.00</p>
+                <p className="opacity-75">{props.title}</p>
                 <p className="opacity-75">{props.category}</p>
             </figcaption>
         </figure>
