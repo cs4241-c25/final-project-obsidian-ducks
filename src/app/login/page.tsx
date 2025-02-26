b08ea06c0f8a16296cccb5777c5105a6b44aa72c
miafrattasio <mefrattasio@wpi.edu>
2025-02-26 12:54:14 -0500

    updated navbar so when you are logged in it shows profile and logout

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -2,14 +2,17 @@
 import LoginForm from "@/components/LoginForm";
 import { useSession } from 'next-auth/react';
 import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
 
 export default function LoginPage() {
     const { data: session, status } = useSession();
     const router = useRouter();
 
-    if (status === 'authenticated') {
-        router.push('/profile');
-    }
+    useEffect(() => {
+        if (status === 'authenticated') {
+            router.push('/profile');
+        }
+    }, [status, router]);
 
     return (
         <main>
