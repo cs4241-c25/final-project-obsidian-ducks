be8ee61ffa0c349b3389fd0d7cfa0378c6803f76
tacheampong <th.acheampong@gmail.com>
2025-02-25 20:08:52 -0500

    fixed button import

--- src/components/Post.tsx
+++ src/components/Post.tsx
@@ -1,5 +1,5 @@
 'use client'
-import Button from "@/components/inputs/Button";
+import Button from "@/components/Button";
 import Image from "next/image";
 import Link from "next/link";
 import {useSession} from "next-auth/react";
