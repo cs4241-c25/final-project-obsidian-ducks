c12942a9125094cc45d88f5a6d9e4b370899c834
nick-leslie <nick.leslie303@gmail.com>
2025-03-02 23:47:53 -0500

    trying to manuly expand the erl flags

--- gleam-chat/entrypoint.sh
+++ gleam-chat/entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Expand ERL_AFLAGS environment variable to include runtime variables
-export ERL_AFLAGS=$(eval echo "$ERL_AFLAGS")
+export ERL_AFLAGS=-proto_dist inet6_tcp -name ${FLY_APP_NAME}-${FLY_IMAGE_REF##*-}@${FLY_PRIVATE_IP} -setcookie "$ERLANG_COOKIE"
+echo $ERL_AFLAGS
 
 exec $@
