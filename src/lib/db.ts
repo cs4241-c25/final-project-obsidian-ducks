4b976e04412a727381e6d246bcc9bb636f9e774a
Andy Truong <andyhtruong01@gmail.com>
2025-03-03 13:14:57 -0500

    Connections to DB no longer established as side effects

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -2,26 +2,20 @@ import mongoose from 'mongoose';
 
 const url = process.env.MONGODB_URI;
 
-//i named the db WPIBuys and made two collections so far: item and user
-//call this function to connect to Database
 async function connectToDatabase(): Promise<void> {
-  if (url === undefined || url === null || url === '') {
-      return;
+    try {
+      if (url === undefined || url === null || url === '') {
+          throw new Error("MongoDB URL must be provided.");
+      }
+      const opts = {
+          bufferCommands: false,
+      };
+      await mongoose.connect(url, opts);
+      console.log("Connected to MongoDB :D");
+  } catch (e) {
+      console.log("Failed to connect to MongoDB D:");
+      console.error(e);
   }
-  const opts = {
-    bufferCommands: false,
-  };
-  await mongoose.connect(url,opts);
 }
-
-connectToDatabase()
-    .then((mongoose) => {
-        console.log(mongoose)
-        console.log("Connected to MongoDB :D");
-    })
-    .catch((e) => {
-        console.log("Failed to connect to MongoDB D:");
-        console.error(e);
-});
 
 export default connectToDatabase;
