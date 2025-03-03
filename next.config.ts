7bec95ebb009409e795d6b754e974c509bb2cc9a
nick-leslie <nick.leslie303@gmail.com>
2025-03-03 11:55:35 -0500

    moved auth to a dif file

--- next.config.ts
+++ next.config.ts
@@ -26,7 +26,7 @@ const nextConfig: NextConfig = {
         // Warning: This allows production builds to successfully complete even if
         // your project has ESLint errors.
         ignoreDuringBuilds: true,
-      },
+    },
     experimental: {
         serverActions: {
             bodySizeLimit: "3mb"
