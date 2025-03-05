c1c7c7021382c9f45640b61930ce990e6a90452c
nick-leslie <nick.leslie303@gmail.com>
2025-03-05 13:37:02 -0500

    realy long message fix

--- src/components/chat/MessageDisplay.tsx
+++ src/components/chat/MessageDisplay.tsx
@@ -4,12 +4,12 @@ import { twMerge } from "tailwind-merge";
 export default function MessageDisplay(props: {username:string,message:ChatMessage}) {
   const sent_by_self = props.message.sender === props.username
   return(
-    <div className={twMerge("w-fit text-xl",sent_by_self ? "" :"place-self-end")}>
+    <div className={twMerge("text-wrap text-xl",sent_by_self ? "" :"place-self-end")}>
       <div className="text-sm">
         {props.message.sender}
       </div>
-      <div className="rounded-sm w-full text-center px-5 py-2 bg-alice-blue-800 ">
-        {props.message.content}
+      <div className="rounded-sm overflow-hidden text-center max-w-5xl px-5 py-2 bg-alice-blue-800 ">
+        <h1 className="text-ellipsis">{props.message.content}</h1>
       </div>
     </div>
   )
